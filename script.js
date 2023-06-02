d51e8f5e924ae6e89e173c61e22af7406752cc0a
joaovitormv <joaovitorjjabuti@gmail.com>
2023-06-02 10:22:14 -0300

    aula 2 javascript

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
 const listElement = document.querySelector("#app ul")
 const inputElement = document.querySelector("#app input")
 const buttonElement = document.querySelector("#app button")
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
 
 console.log(listElement, inputElement, buttonElement)
 
-const tarefas = ["estudar para prova", "dormir cedo", "KKKKKKKKKKK"]
+/*const tarefas = ["estudar para prova", "dormir cedo", "programar"]*/
 
-for (const itemTarefa of tarefas) {
-    const tarefaElement = document.createElement("li")
-    const tarefaText = document.createTextNode(itemTarefa)
-    tarefaElement.appendChild(tarefaText)
-    listElement.appendChild(tarefaElement)
+function rendertarefas(){
+    listElement.innerHTML = ""
+    for (const itemTarefa of tarefas) {
+        const tarefaElement = document.createElement("li")
+        const tarefaText = document.createTextNode(itemTarefa)
+        const linkElement = document.createElement("a")
+        linkElement.setAttribute("href", "#")
+        const pos = tarefas.indexOf(itemTarefa)
+        linkElement.setAttribute("onclick", `deleteTarefa(${pos})`)
+        const linkText = document.createTextNode("Excluir")
+        linkElement.appendChild(linkText)
+
+        tarefaElement.appendChild(tarefaText)
+        tarefaElement.appendChild(linkElement)
+        listElement.appendChild(tarefaElement)
+    }
 }
 
+rendertarefas()
+
+function addTarefas(){
+    const text = inputElement.value
+    tarefas.push(text)
+    rendertarefas()
+    inputElement.value = ""
+    savetostorage()
+}
+
+
+
+buttonElement.onclick = addTarefas
+
+
+function deleteTarefa(pos){
+    tarefas.splice(pos, 1)
+    rendertarefas()
+    savetostorage()
+}
+
+function savetostorage(){
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+}
